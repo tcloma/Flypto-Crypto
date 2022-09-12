db89bd69903b2b8712513b5bef8a6b2ea0c9236c
Hammad <hammadfaiz172@gmail.com>
2022-09-12 15:41:45 -0400

    table-relations

--- app/models/purchased_coin.rb
+++ app/models/purchased_coin.rb
@@ -1,2 +1,4 @@
 class PurchasedCoin < ApplicationRecord
+  has_many :position_lists
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :watch_lists
+  has_many :purchased_coins
+  has_many :position_lists, through: :purchased_coins
 end
