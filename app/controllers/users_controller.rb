f20a4cb91412458bb95d9783d64328eb8b161eb1
Hammad <hammadfaiz172@gmail.com>
2022-09-12 18:08:05 -0400

    adding random

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    render json: user
+  end
+
+  private
+  def user_params 
+    params.permit(:name, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # User.rb routes
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  get '/me', to: 'users#show'
   post '/users', to: 'users#create'     # params : name, password, email
   patch '/users/:id', to: 'users#update'    # params : funds
+
+  post '/login', to: 'sessions#create'
 
   # PurchasedCoins.rb routes
   get '/purchasedcoins', to: 'purchasedcoins#index'
