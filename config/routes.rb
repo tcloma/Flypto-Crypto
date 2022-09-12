600cc68ce22f06d10a2130d543f3b900904bf84a
Olivia <oliviatmanalastas@gmail.com>
2022-09-12 07:13:46 -0700

    Added routes to user, purchasedCoins, and Watchlists

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   get '/purchasedcoins', to: 'purchasedcoins#index'
   patch '/purchasedcoins/:id', to: 'purchasedcoins#update'    #update the quantity when you purchase
   delete '/purchasedcoins/:id', to: 'purchasedcoins#destroy'
+
+  # WatchList.rb routes
+  get '/watchlists', to: 'watchlists#index'
+  get '/watchlists/:id', to: 'watchlists#show'
+  delete '/watchlists/:id', to: 'watchlist#destroy'
+
   
   #
 end
