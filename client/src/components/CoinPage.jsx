58e75913b31763a7e38df9b058fa688cfe8f8352
George <gtduncan@usc.edu>
2022-09-12 17:43:16 -0400

    functional toggle

--- client/src/components/CoinPage.jsx
+++ client/src/components/CoinPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState, React} from 'react';
 import ReactDOM from 'react-dom';
 import FusionCharts from 'fusioncharts';
 import Charts from 'fusioncharts/fusioncharts.charts';
@@ -11,16 +11,29 @@ import { getCoin, getCoinGraphData } from '../coinApi';
 ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
 
 const CoinPage = ({selectedCoin}) => {
-  
-  const { isLoading: coinLoading, data: specCoinData } = useQuery('coinData', () => getCoin(selectedCoin));
-  const { isLoading: graphLoading, data: graphData } = useQuery('graph', () => getCoinGraphData(selectedCoin));
 
+  const [timePeriod, setTimePeriod] = useState('d1')
+
+  const { isLoading: coinLoading, data: specCoinData } = useQuery('coinData', () => getCoin(selectedCoin));
+  const { isLoading: graphLoading, data: graphData, refetch} = useQuery(timePeriod, () => getCoinGraphData(selectedCoin, timePeriod));
 
   const coinData = graphData
 
   const newData = coinData?.map((day) => {
     const newObj = {};
-    const date = moment(day.date).format('DD MMM, YYYY');
+    let date;
+    if(timePeriod === 'm1')
+    {
+        date = moment(day.date).format('LT');
+    }
+    if(timePeriod === 'h1')
+    {
+        date = moment(day.date).format('MMM DD');
+    }
+    if(timePeriod === 'd1')
+    {
+        date = moment(day.date).format('MMM DD, YYYY');
+    }
     newObj['label'] = date
     newObj['value'] = day.priceUsd
     return newObj
@@ -36,7 +49,7 @@ const CoinPage = ({selectedCoin}) => {
       showPlotBorder: true,
       plotBorderThickness: 3,
       setAdaptiveYMin: true,
-      labelStep: 50,
+      labelStep: 80,
       bgColor: '#222222',
       subCaption: `(${specCoinData?.symbol})`,
       xAxisName: 'Day',
@@ -49,7 +62,7 @@ const CoinPage = ({selectedCoin}) => {
 
   const chartConfigs = {
     type: 'area2d',
-    width: 1400,
+    width: 1800,
     height: 500,
     dataFormat: 'json',
     dataSource: dataSource
@@ -60,6 +73,14 @@ const CoinPage = ({selectedCoin}) => {
       <div className='coin-chart'>
         {graphLoading ? <p>Loading ...</p> : <ReactFC {...chartConfigs} />}
       </div>
+    <div className='time-choices'>
+        <label htmlFor="day">Today</label>
+        <input onClick={()=> {setTimePeriod('m1'); refetch()}} type="radio" id="day" name="time-period-setter" value="day"/>
+        <label htmlFor="month">This Month</label>
+        <input onClick={()=> {setTimePeriod('h1'); refetch()}} type="radio" id="month" name="time-period-setter" value="month"/>
+        <label htmlFor="year">This Year</label>
+        <input onClick={()=> {setTimePeriod('d1'); refetch()}} type="radio" id="year" name="time-period-setter" value="year"/>
+    </div>
     </div>
   )
 }
