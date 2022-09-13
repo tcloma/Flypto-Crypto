660ee98ce31f1b1d47b88a53cbcdbc276d7c8f16
Hammad <hammadfaiz172@gmail.com>
2022-09-13 10:29:20 -0400

    Some more routes

--- app/controllers/purchased_coins_controller.rb
+++ app/controllers/purchased_coins_controller.rb
@@ -1,5 +1,16 @@
 class PurchasedCoinsController < ApplicationController
+  
   def index
     render json: PurchasedCoin.all
   end
+
+  def show 
+    user = User.find_by(id: params[:id])
+    if user 
+      render json: user, status: :found
+    else
+      render json: {error: "Purchased_Coin not found"}, status: :Not_Found
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,16 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'     # params : name, password, email
   patch '/users/:id', to: 'users#update'    # params : funds
 
-
   # Login and Logout Routes
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+
   # PurchasedCoins.rb routes
-  get '/purchasedcoins', to: 'purchasedcoins#index'
-  patch '/purchasedcoins/:id', to: 'purchasedcoins#update'    #update the quantity when you purchase
+  get '/purchasedcoins', to: 'purchased_coins#index'
+  get '/purchasedcoins/:id', to: 'purchased_coins#show'
+  # post '/'
+  patch '/purchasedcoins/:id', to: 'purchased_coins#update'    #update the quantity when you purchase
   delete '/purchasedcoins/:id', to: 'purchasedcoins#destroy'
 
   # WatchList.rb routes
