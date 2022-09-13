17cb0f5ada515b92b73b0dd553ccdf69cc19510f
Hammad <hammadfaiz172@gmail.com>
2022-09-13 14:54:19 -0400

    cruds mostly done

--- app/controllers/position_lists_controller.rb
+++ app/controllers/position_lists_controller.rb
@@ -1,2 +1,31 @@
 class PositionListsController < ApplicationController
+  def index
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user.position_lists.all, status: :found
+    else  
+      render json: {error: "Position_List not found"}, status: :not_found
+    end
+  end
+
+  def show 
+    position_list = PositionList.find_by(id: params[:id])
+    if position_list
+      render json: position_list, status: :found
+    else 
+      render json: {error: "Position_List not found"}, status: :not_found
+    end
+  end
+
+  def create 
+    position = PositionList.create(post_params)
+    render json: position, status: :created
+  end
+
+  private 
+
+  def post_params
+    params.permit(:time_of_purchase, :price_of_purchase, :purchased_coin_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,32 @@ Rails.application.routes.draw do
   # User.rb routes
   get '/users', to: 'users#index'
   get '/me', to: 'users#show'       # staying logged in 
-  post '/signup', to: 'users#create'     # params : name, password, email
-  patch '/users/:id', to: 'users#update'    # params : funds
+  post '/signup', to: 'users#create'     # params : name, password, email, funds
+  patch '/users', to: 'users#update'    # params : funds
 
   # Login and Logout Routes
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+    # WatchList.rb routes
+  get '/watchlists', to: 'watch_lists#index'
+  get '/watchlists/:id', to: 'watch_lists#show'
+  post '/watchlists', to: 'watch_lists#create'
+  delete '/watchlists/:id', to: 'watch_lists#destroy'
+
 
   # PurchasedCoins.rb routes
   get '/purchasedcoins', to: 'purchased_coins#index'
   get '/purchasedcoins/:id', to: 'purchased_coins#show'
-  # post '/'
+  post '/purchasedcoins', to: "purchased_coins#create"
   patch '/purchasedcoins/:id', to: 'purchased_coins#update'    #update the quantity when you purchase
-  delete '/purchasedcoins/:id', to: 'purchasedcoins#destroy'
+  delete '/purchasedcoins/:id', to: 'purchased_coins#destroy'
+  
 
-  # WatchList.rb routes
-  get '/watchlists', to: 'watchlists#index'
-  get '/watchlists/:id', to: 'watchlists#show'
-  delete '/watchlists/:id', to: 'watchlist#destroy'
+  # PositionList.rb routes
+  get '/positionlists', to: "position_lists#index"
+  get '/positionlists/:id', to: "position_lists#show"
+  post '/positionlists', to: "position_lists#create"
 
   
 end
