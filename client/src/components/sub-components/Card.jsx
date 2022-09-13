9521b5b8feaa9a17b81419280fe875a44eeb4f33
Tyrone <tyrone.cloma0219@gmail.com>
2022-09-13 14:24:55 -0400

    redid css on some components and added crypto page rendering

--- client/src/components/sub-components/Card.jsx
+++ client/src/components/sub-components/Card.jsx
@@ -1,31 +1,32 @@
 import { useNavigate } from "react-router-dom";
 
+export const decimalCheck = (value) => {
+  if (value <= 2) {
+    return Number.parseFloat(value).toFixed(5)
+  }
+  else if (value <= 100) {
+    return Number.parseFloat(value).toFixed(2)
+  }
+  else {
+    return Number.parseFloat(value).toFixed()
+  }
+}
 
-const Card = ({ name, price, id, setSelectedCoin }) => {
+const Card = ({ name, price, id, setSelectedCoin, change24Hr }) => {
   let navigate = useNavigate();
 
+  console.log(change24Hr)
   const getCoin = () => {
     setSelectedCoin(id)
     console.log(id)
     navigate('/trade')
   } 
 
-  const decimalCheck = (value) => {
-    if (value <= 2) {
-      return Number.parseFloat(value).toFixed(5)
-    }
-    else if (value <= 100) {
-      return Number.parseFloat(value).toFixed(2)
-    }
-    else {
-      return Number.parseFloat(value).toFixed()
-    }
-  }
-
   return (
     <div className="card" onClick={getCoin}>
       <h3> {name} </h3>
       <p> ${decimalCheck(price)} </p>
+      {change24Hr !== undefined ? <p> { parseFloat(change24Hr).toFixed(2) } % </p> : null }
     </div>
   )
 }
