37ad4153358e211bbc78ebfedec7f15ca13bf290
Tyrone <tyrone.cloma0219@gmail.com>
2022-11-19 16:59:01 -0500

    fixed profile page loading error

--- client/src/components/ProfilePage.jsx
+++ client/src/components/ProfilePage.jsx
@@ -6,81 +6,81 @@ import { getAllCoins } from '../apis/coinApi'
 import Table from './sub-components/Table'
 import { useEffect } from 'react'
 
-const ProfilePage = ({ user, userCoins, setSelectedCoin }) => {
-  const { status, error, data: allCoins } = useQuery('coins', () => getAllCoins())
-  const filteredCoins = allCoins?.filter(coin => userCoins.map(coin => coin.name).includes(coin.name.toLowerCase()))
-  
-  const findQuantity = (searchCoin) => {
-    const matchedCoin = userCoins.find(coin => coin.name === searchCoin)
-    return matchedCoin.quantity
-  }
-  
-  const filteredWithQuantity = filteredCoins?.map(coin => ({...coin, quantity: findQuantity(coin.name.toLowerCase())}))
+const ProfilePage = ({ user, userCoins = [], setSelectedCoin }) => {
+   const { status, error, data: allCoins } = useQuery('coins', () => getAllCoins())
+   const filteredCoins = allCoins?.filter(coin => userCoins.map(coin => coin.name).includes(coin.name.toLowerCase()))
 
-  console.log('user',userCoins)
-  console.log(filteredWithQuantity)
+   const findQuantity = (searchCoin) => {
+      const matchedCoin = userCoins.find(coin => coin.name === searchCoin)
+      return matchedCoin.quantity
+   }
 
-  const [gaining, setGaining] = useState(false)
+   const filteredWithQuantity = filteredCoins?.map(coin => ({ ...coin, quantity: findQuantity(coin.name.toLowerCase()) }))
 
-  let totalChange = 0
-  filteredCoins?.forEach(coin => {
-    totalChange = totalChange + parseFloat(coin.changePercent24Hr)
-  })
+   console.log('user', userCoins)
+   console.log(filteredWithQuantity)
 
-  const gainCheck = () => {
-    if (totalChange > 0) {
-      setGaining(true)
-    } else if (totalChange < 0) {
-      setGaining(false)
-    }
-  }
+   const [gaining, setGaining] = useState(false)
 
-  useEffect(() => {
-    gainCheck()
-  }, [])
+   let totalChange = 0
+   filteredCoins?.forEach(coin => {
+      totalChange = totalChange + parseFloat(coin.changePercent24Hr)
+   })
 
-  return (
-    <div className="profile-container">
-      <div className="portfolio-page">
-        <div className='portfolio-header'>
-          <div className='user-info'>
-            <h1> {user?.name}'s Portfolio </h1>
-            <p> {user?.email} </p>
-            <p> <span className='money'>$</span>{user?.funds}</p>
-          </div>
-          <div className='portfolio-graph'>
-            <h3> Your portfolio is {gaining ? 'up' : 'down'}: </h3>
-            <h2 className={gaining ? 'total-gaining' : 'total-losing'}>
-              {gaining ? '▲' : '▼'}{totalChange < 0 ? totalChange?.toFixed(2) * -1 : totalChange?.toFixed(2)}%
-            </h2>
-          </div>
-        </div>
+   const gainCheck = () => {
+      if (totalChange > 0) {
+         setGaining(true)
+      } else if (totalChange < 0) {
+         setGaining(false)
+      }
+   }
 
-        <div className='watchlist-container'>
-          <h2 style={{ color: 'white' }}> Tracked Coins: </h2>
-          {filteredCoins?.slice(0,5).map(coin => {
-            return (
-              <Card
-                setSelectedCoin={setSelectedCoin}
-                id={coin.id}
-                key={coin.id}
-                name={coin.name}
-                symbol={coin.symbol}
-                price={coin.priceUsd}
-              />
-            )
-          })}
-        </div>
-        <div className='owned-coins-table'>
-          <h2> Owned Coins: </h2>
-          <Table
-            allCoins={filteredWithQuantity}
-            setSelectedCoin={setSelectedCoin}
-            withQuantity ={true}
-          />
-        </div>
+   useEffect(() => {
+      gainCheck()
+   }, [])
+
+   return (
+      <div className="profile-container">
+         <div className="portfolio-page">
+            <div className='portfolio-header'>
+               <div className='user-info'>
+                  <h1> {user?.name}'s Portfolio </h1>
+                  <p> {user?.email} </p>
+                  <p> <span className='money'>$</span>{user?.funds}</p>
+               </div>
+               <div className='portfolio-graph'>
+                  <h3> Your portfolio is {gaining ? 'up' : 'down'}: </h3>
+                  <h2 className={gaining ? 'total-gaining' : 'total-losing'}>
+                     {gaining ? '▲' : '▼'}{totalChange < 0 ? totalChange?.toFixed(2) * -1 : totalChange?.toFixed(2)}%
+                  </h2>
+               </div>
+            </div>
+
+            <div className='watchlist-container'>
+               <h2 style={{ color: 'white' }}> Tracked Coins: </h2>
+               {filteredCoins?.slice(0, 5).map(coin => {
+                  return (
+                     <Card
+                        setSelectedCoin={setSelectedCoin}
+                        id={coin.id}
+                        key={coin.id}
+                        name={coin.name}
+                        symbol={coin.symbol}
+                        price={coin.priceUsd}
+                     />
+                  )
+               })}
+            </div>
+            <div className='owned-coins-table'>
+               <h2> Owned Coins: </h2>
+               <Table
+                  allCoins={filteredWithQuantity}
+                  setSelectedCoin={setSelectedCoin}
+                  withQuantity={true}
+               />
+            </div>
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 export default ProfilePage;
--- client/src/components/sub-components/Header.jsx
+++ client/src/components/sub-components/Header.jsx
@@ -3,52 +3,52 @@ import { Link } from "react-router-dom";
 const Header = ({ user, setUser }) => {
 
 
-  // const handleLogoutClick = () => {
-  //   fetch('/logout', {
-  //     method: 'DELETE',
-  //   }).then(() => onLogout())
-  //   console.log(onLogout())
-  // }
+   // const handleLogoutClick = () => {
+   //   fetch('/logout', {
+   //     method: 'DELETE',
+   //   }).then(() => onLogout())
+   //   console.log(onLogout())
+   // }
 
-  const handleLogoutClick = () => {
-    fetch("/logout", { method: "DELETE" }).then((r) => {
-      if (r.ok) {
-        setUser({})
-      }
-    });
-  }
+   const handleLogoutClick = () => {
+      fetch("/logout", { method: "DELETE" }).then((r) => {
+         if (r.ok) {
+            setUser({})
+         }
+      });
+   }
 
-  console.log('Current User: ', { name: user?.name, email: user?.email, funds: user?.funds })
+   console.log('Current User: ', { name: user?.name, email: user?.email, funds: user?.funds })
 
-  return (
-    <div className='header'>
-      <Link to='/'>
-        <div className="logo-container">
-          <p style={{ fontSize: '1.5em' }}> 🪙Flypto </p>
-        </div>
-      </Link>
-      <div className="nav-buttons">
-        <Link to='/trade'>
-          <p> Trade </p>
-        </Link>
-        <Link to='/crypto'>
-          <p> Crypto </p>
-        </Link>
-        {user && <Link to='/profile'>
-          <p> Profile </p>
-        </Link>}
+   return (
+      <div className='header'>
+         <Link to='/'>
+            <div className="logo-container">
+               <p style={{ fontSize: '1.5em' }}> 🪙Flypto </p>
+            </div>
+         </Link>
+         <div className="nav-buttons">
+            <Link to='/trade'>
+               <p> Trade </p>
+            </Link>
+            <Link to='/crypto'>
+               <p> Crypto </p>
+            </Link>
+            {user.email && <Link to='/profile'>
+               <p> Profile </p>
+            </Link>}
 
 
-        {user.email ?
-          <Link to='/'>
-            <p onClick={handleLogoutClick}>Logout</p>
-          </Link>
-          :
-          <Link to='/login'>
-            <p> Login </p>
-          </Link>}
+            {user.email ?
+               <Link to='/'>
+                  <p onClick={handleLogoutClick}>Logout</p>
+               </Link>
+               :
+               <Link to='/login'>
+                  <p> Login </p>
+               </Link>}
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 export default Header;
